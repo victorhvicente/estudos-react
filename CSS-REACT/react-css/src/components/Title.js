68672a5edb7ec6fd9ac902a4d34c77c91aafbe73
vhvicente99@gmail.com <vhvicente99@gmail.com>
2025-08-12 20:24:12 -0300

    react/css

--- CSS-REACT/react-css/src/components/Title.js
+++ CSS-REACT/react-css/src/components/Title.js
@@ -1,12 +1,19 @@
+// Importa o arquivo de estilos CSS como um módulo, permitindo usar classes de forma isolada
+// 'styles' será um objeto onde cada chave corresponde a uma classe definida no CSS
 import styles from './Title.module.css';
 
+// Declaração do componente funcional Title
 const Title = () => {
 
-    return(
+    return (
         <div>
+            {/* h1 usando uma classe vinda do CSS Module.
+                O 'styles.my_title' garante que o nome da classe será único
+                e não entrará em conflito com outros estilos da aplicação */}
             <h1 className={styles.my_title}>Utilizando CSS MODULES</h1>
         </div>
     );
 }
 
+// Exporta o componente para uso em outros arquivos
 export default Title;
