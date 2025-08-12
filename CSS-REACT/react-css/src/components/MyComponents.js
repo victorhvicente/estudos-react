3e002c5bcdf3a50380eae36f4ad3721211694a6e
vhvicente99@gmail.com <vhvicente99@gmail.com>
2025-08-12 20:23:06 -0300

    react/css

--- CSS-REACT/react-css/src/components/MyComponents.js
+++ CSS-REACT/react-css/src/components/MyComponents.js
@@ -1,13 +1,19 @@
+// Importa o arquivo de estilo CSS específico para este componente
 import './MyComponents.css';
 
+// Declaração do componente funcional MyComponents
 const MyComponents = () => {
-    return(
+    return (
         <div>
+            {/* Título principal que usa o estilo padrão do componente */}
             <h1>Meu componente</h1>
+
+            {/* Título com uma classe CSS chamada 'sem-interferir',
+                que aplica um estilo específico sem afetar outros elementos */}
             <h1 className="sem-interferir">CSS sem interferir</h1>
         </div>
     );
-
 }
 
+// Exporta o componente para que possa ser utilizado em outros arquivos
 export default MyComponents;
