2ed869189b19b954f3ab68985e6c06105fe797f7
Victor Hugo Vicente <157495513+victorhvicente@users.noreply.github.com>
2025-08-14 14:31:19 -0300

    Update Container.js

--- AVANCANDO-REACT/avancando/src/components/Container.js
+++ AVANCANDO-REACT/avancando/src/components/Container.js
@@ -1,11 +1,18 @@
-const Container = ({children, myValue}) => {
+// Componente funcional Container que recebe props: children e myValue
+const Container = ({ children, myValue }) => {
     return (
         <div>
+            {/* Título fixo do componente */}
             <h1>Este é um Container</h1>
+
+            {/* Renderiza o conteúdo filho passado entre as tags do componente */}
             {children}
+
+            {/* Exibe o valor recebido através da prop 'myValue' */}
             <p>O valor é: {myValue}</p>
         </div>
     );
 }
 
+// Exporta o componente para ser utilizado em outros arquivos
 export default Container;
